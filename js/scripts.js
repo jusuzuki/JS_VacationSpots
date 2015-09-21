cd6bfd7ee79e9124f2abf533841e7899a171ccdd
morgan lutz & juliana suzuki <student@epicodus.com>
2015-09-21 16:33:17 -0700

    declare global variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 var geocoder;
- var map;
+var map;
+var address;
+var inputtedCountryName;
+var inputtedDate;
+var inputtedNote;
+
  function initialize() {
    geocoder = new google.maps.Geocoder();
    var latlng = new google.maps.LatLng(-34.397, 150.644);
@@ -11,11 +16,14 @@ var geocoder;
  }
 
 function codeAddress() {
-   var address = $("input#new-location-name").val();
+  debugger;
+
    geocoder.geocode( { 'address': address}, function(results, status) {
      if (status == google.maps.GeocoderStatus.OK) {
        map.setCenter(results[0].geometry.location);
-       var contentString = address;
+       var contentString = '<strong>' + address + '</strong>' + '<br>'
+       + inputtedCountryName + '<br>' + inputtedDate + '<br>' + inputtedNote
+       + "<span class='vacation'></span>";
 
 
        var infowindow = new google.maps.InfoWindow({
@@ -30,8 +38,9 @@ function codeAddress() {
 
        marker.addListener('click', function() {
          infowindow.open(map, marker);
-         $("#show-location").show();
+
        });
+
      } else {
        alert("Geocode was not successful for the following reason: " + status);
      }
@@ -50,14 +59,14 @@ $(document).ready(function() {
 
   $("form#new-spot").submit(function(event) {
       event.preventDefault();
-
-      var inputtedCountryName = $("input#new-country-name").val();
-      var inputtedLocationName = $("input#new-location-name").val();
-      var inputtedDate = $("input#new-time").val();
-      var inputtedNote = $("input#new-note").val();
-
-      var newSpot = { countryName: inputtedCountryName, locationName: inputtedLocationName, date: inputtedDate, notes: inputtedNote, landmarks: [] };
 debugger;
+      inputtedCountryName = $("input#new-country-name").val();
+      address = $("input#new-location-name").val();
+      inputtedDate = $("input#new-time").val();
+      inputtedNote = $("input#new-note").val();
+
+      var newSpot = { countryName: inputtedCountryName, locationName: address, date: inputtedDate, notes: inputtedNote, landmarks: [] };
+
       $(".new-landmark").each(function() {
         var inputtedLandmark = $(this).find("input.new-nameOf").val();
         var newLandmark = { nameOf: inputtedLandmark };
@@ -65,12 +74,13 @@ debugger;
         newSpot.landmarks.push(newLandmark);
       });
 
+      codeAddress();
 
-      $("ul#vacation-spots").append("<li><span class='vacation'>" + newSpot.locationName + "</span></li>");
+     $("ul#vacation-spots").append("<li><span class='vacation'>" + newSpot.locationName + "</span></li>");
 
      $(".vacation").last().click(function() {
        $("#show-location").show();
-debugger;
+
        $("#show-location h2").text(newSpot.locationName);
        $(".country").text(newSpot.countryName);
        $(".time").text(newSpot.date);
